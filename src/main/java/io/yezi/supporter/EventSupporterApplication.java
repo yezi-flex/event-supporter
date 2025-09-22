9f85bc82656120bf3cbf35d660cfb814f70ddc35
yezi-flex <yeziflex23@gmail.com>
2025-09-22 16:51:27 +0900

    initial-commit

---
+++ src/main/java/io/yezi/supporter/EventSupporterApplication.java
@@ -0,0 +1,13 @@
+package io.yezi.supporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSupporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventSupporterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/yezi/supporter/EventSupporterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.yezi.supporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSupporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
