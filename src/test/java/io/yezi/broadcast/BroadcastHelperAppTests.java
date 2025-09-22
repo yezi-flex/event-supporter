84e92c82d30adb5e888dbaf3f54a16a82c5714d1
yezi-flex <yeziflex23@gmail.com>
2025-09-23 01:12:13 +0900

    chore: @SpringBootTest 실행 시 test profile 적용

--- src/test/java/io/yezi/broadcast/BroadcastHelperAppTests.java
+++ src/test/java/io/yezi/broadcast/BroadcastHelperAppTests.java
@@ -3,8 +3,10 @@ package io.yezi.broadcast;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import io.yezi.broadcast.config.TestBase;
+
 @SpringBootTest
-class BroadcastHelperAppTests {
+class BroadcastHelperAppTests extends TestBase {
 
 	@Test
 	void contextLoads() {
