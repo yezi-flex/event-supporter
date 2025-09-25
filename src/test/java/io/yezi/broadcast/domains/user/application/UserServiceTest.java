79356e721c33fe879dd8ddb8f9ce0c050f983965
yezi-flex <yeziflex23@gmail.com>
2025-09-25 17:28:27 +0900

    test: 사용자 애플리케이션 서비스 단위 테스트 구현
    사용자 회원가입 서비스의 단위 테스트를 구현합니다.
    
    ## 주요 변경사항
    
    ### Application Tests
    - UserServiceTest: 사용자 서비스 단위 테스트
    
    ## 테스트 커버리지
    - 정상적인 회원가입 시나리오
    - 중복 사용자명 검증 실패 시나리오
    - Mock 객체를 활용한 의존성 격리 테스트
    
    ## 구현 특징
    - Mockito를 활용한 의존성 격리
    - BDD 스타일 테스트 작성
    - 예외 상황에 대한 테스트 케이스 포함

---
+++ src/test/java/io/yezi/broadcast/domains/user/application/UserServiceTest.java
@@ -0,0 +1,82 @@
+package io.yezi.broadcast.domains.user.application;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import io.yezi.broadcast.config.base.ServiceTestBase;
+import io.yezi.broadcast.domains.user.application.dto.SignUpRequest;
+import io.yezi.broadcast.domains.user.application.policy.UserValidatePolicy;
+import io.yezi.broadcast.domains.user.domain.UserFixtureGenerator;
+import io.yezi.broadcast.domains.user.domain.model.User;
+import io.yezi.broadcast.domains.user.domain.repository.UserRepo;
+
+@DisplayName("Service:User")
+class UserServiceTest extends ServiceTestBase {
+
+	@Mock
+	private UserRepo userRepo;
+
+	@Mock
+	private UserValidatePolicy userValidatePolicy;
+
+	@InjectMocks
+	private UserService userService;
+
+	@Nested
+	@DisplayName("회원 생성")
+	class createTest {
+
+		@Test
+		@DisplayName("중복된 정보가 없는 경우 회원을 생성한다.")
+		void create() {
+			// Given
+			var given = UserFixtureGenerator.회원_가입_요청_객체_생성();
+			stubSaveReturnsEntityWithId();
+
+			// When
+			Long actual = userService.create(given);
+
+			// Then
+			assertThat(actual).isEqualTo(UserFixtureGenerator.ID);
+			ArgumentCaptor<User> userCaptor = ArgumentCaptor.forClass(User.class);
+			then(userRepo).should().save(userCaptor.capture());
+
+			assertUserSavedWith(userCaptor.getValue(), given);
+		}
+
+		private void stubSaveReturnsEntityWithId() {
+			BDDMockito.given(userRepo.save(any(User.class)))
+				.willAnswer(invocation -> UserFixtureGenerator.식별자가_존재하는_사용자_도메인_객체_생성());
+		}
+
+		private void assertUserSavedWith(User actual, SignUpRequest expected) {
+			assertThat(actual.getUsername()).isEqualTo(expected.username());
+		}
+
+		@Test
+		@DisplayName("[예외]사용자 아이디가 중복된 경우")
+		void throwException_WhenDuplicatedUsername() {
+			// Given
+			var given = UserFixtureGenerator.회원_가입_요청_객체_생성();
+
+			// When
+			willThrow(new IllegalArgumentException())
+				.given(userValidatePolicy)
+				.validate(given);
+
+			// Then
+			assertThatExceptionOfType(IllegalArgumentException.class)
+				.isThrownBy(() -> userService.create(given));
+			then(userRepo).shouldHaveNoInteractions();
+		}
+	}
+}
